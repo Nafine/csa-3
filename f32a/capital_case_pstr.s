360eb36de5ff18a84820b6269b665cc5a5f697b7
nafine <pavel.nafine@gmail.com>
2026-03-08 15:26:17 +0300

    feat: fix size rewrite

--- f32a/capital_case_pstr.s
+++ f32a/capital_case_pstr.s
@@ -1,8 +1,8 @@
     .data
 
 buf:             .byte  32, '________________________________'
-max_size_dec:    .word  -33
 actual_buf_size: .word  0
+max_size_dec:    .word  -33
 const_newline:   .word  0x0a
 input_addr:      .word  0x80
 output_addr:     .word  0x84
@@ -20,57 +20,70 @@ _start:
     halt
 
 read_line:
-    lit buf b!               \ b for buf address
-    @ lit 255 and            \ input_size:[]
-
-    dup
-    @p max_size_dec +
-    -if error
+    lit buf lit 1 + b!       \ b for buf address
+    lit 1                    \ shift:[]
+    @ lit 255 and            \ input_size:shift:[]
 read_line_while:
-    dup                      \ input_size:input_size:[]
-    if error                 \ input_size:[]
+    dup                      \ input_size:input_size:shift:[]
+    if error                 \ input_size:shift:[]
 
-    @ lit 255 and            \ cur_sym:input_size:[]
+    @ lit 255 and            \ cur_sym:input_size:shift:[]
 
-    dup
+    dup                      \ cur_sym:cur_sym:input_size:shift:[]
     @p const_newline xor     \ check whether symbol is newline character
     if read_line_finish      \ finish reading if symbol was newline character
 
-    !b                       \ input_size:[]
+    !b                       \ input_size:shift:[]
 
-    lit -1 +                 \ input_size - 1:[]
+    lit -1 +                 \ input_size - 1:shift:[]
 
-    @p actual_buf_size lit 1 + \ actual_buf_size + 1:input_size:[]
+    over lit 1 +             \ shift + 1:input_size - 1:[]
 
-    dup                      \ actual_buf_size + 1:actual_buf_size + 1:input_size:[]
-    lit buf + b!             \ actual_buf_size + buf_address:input_size:[]
+    dup                      \ shift + 1:shift + 1:input_size:[]
+    lit buf + b!             \ move pointer, shift:input_size:[]
 
-    !p actual_buf_size       \ input_size:[]
+    over                     \ input_size:shift[]
 
     read_line_while ;
 read_line_finish:
+    drop                     \ input_size:shift:[]
+    drop                     \ shift:[]
+
+    lit -1 +                 \ shift - 1:[]
+
     lit buf a!
+
+    rewrite_size
+    ;
+
+rewrite_size:
+    lit buf b!
+
+    @ lit 0xffffff00         \ mask:first_byte:size:[]
+    and                      \ masked_byte:size:[]
+    xor !b                   \ rewrite first byte
     ;
 
 write_output:
     @p output_addr b!
-    @p actual_buf_size
+    @+ lit 255 and           \ buf_size:[]
 write_output_while:
-    dup
-    if write_output_finish
+    dup                      \ buf_size:buf_size:[]
+    if write_output_finish   \ buf_size:[]
 
-    @+ lit 255 and
+    @+ lit 255 and           \ cur_sym:buf_size:[]
 
-    !b
+    !b                       \ buf_size:[]
 
-    lit -1 +
+    lit -1 +                 \ buf_size - 1:[]
+
     write_output_while ;
 
 write_output_finish:
+    drop                     \ remove remaining zero counter
     ;
 
 error:
     @p output_addr b!
-    lit -1 !b
+    lit -1 !b                \ write -1 to output
     halt
-
