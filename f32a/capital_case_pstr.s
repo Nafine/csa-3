ac839e33c6304b3fb65b11a305bec9107c34bc83
nafine <pavel.nafine@gmail.com>
2026-03-08 21:18:56 +0300

    feat: f32a done

--- f32a/capital_case_pstr.s
+++ f32a/capital_case_pstr.s
@@ -1,94 +1,166 @@
     .data
 
 buf:             .byte  31, '_______________________________'
-capitalized_str: .byte  31, '_______________________________'
 
 const_newline:   .word  0x0a
 const_space:     .word  0x20
+
+const_a_neg:     .word  -97
+const_z:         .word  122
+const_A_neg:     .word  -65
+const_Z:         .word  90
+
+space_seen:      .word  1
 
 input_addr:      .word  0x80
 output_addr:     .word  0x84
 
     .text
     .org 0x100
-_start:
-    @p input_addr a!         \ a for input
 
+    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+_start:
     read_line
+
+    title
 
     write_output
 
     halt
 
+    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
 read_line:
-    lit buf lit 1 + b!       \ b for buf address
-    lit 1                    \ shift:[]
-    lit 31                   \ buffer_remaining:shift:[]
+    lit buf a!               \ a for output
+    @p input_addr b!         \ b for input
 read_line_while:
-    dup                      \ buffer_remaining:buffer_remaining:shift:[]
-    if error                 \ buffer_remaining:shift:[]
+    @b lit 255 and           \ cur_sym:[]
 
-    @ lit 255 and            \ cur_sym:buffer_remaining:shift:[]
-
-    dup                      \ cur_sym:cur_sym:buffer_remaining:shift:[]
+    dup                      \ cur_sym:cur_sym:[]
     @p const_newline xor     \ check whether symbol is newline character
-    if read_line_finish      \ finish reading if symbol was newline character
+    if read_line_finish
+
+    a lit -31 +
+    if error
+
+    lit 1 a + a!
 
     write_first_byte
-
-    lit -1 +                 \ buffer_remaining - 1:shift:[]
-
-    over lit 1 +             \ shift + 1:buffer_remaining - 1:[]
-
-    dup                      \ shift + 1:shift + 1:buffer_remaining:[]
-    lit buf + b!             \ move pointer, shift:buffer_remaining:[]
-
-    over                     \ buffer_remaining:shift[]
 
     read_line_while ;
 read_line_finish:
-    drop                     \ buffer_remaining:shift:[]
-    drop                     \ shift:[]
+    drop                     \ []
 
-    lit -1 +                 \ shift - 1:[]
-
-    lit buf b!
+    a
+    lit buf a!
 
     write_first_byte
-
-    lit buf a!
     ;
 
-\ since current arch is little-endian, i.e., mem[0x00]=0x0f, mem[0x01]=0x0e, mem[0x02]=0x0d, mem[0x03]=0x0c 
-\ will become 0x0c0d0e0f on read from address 0x00
 
-write_first_byte:
-    lit 0xffffff00 @b                       \ b_value:mask:input:[]
+    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-    and                      \ masked_value:input:[]
-    xor !b                   \ rewrite first byte
+title:
+    lit buf a!               \ a for input
+
+    @+ lit 255 and >r          \ input_remaining:[]
+title_while:
+    @ lit 255 and dup        \ cur_sym:cur_sym:input_remaining:[]
+
+    \ branch if cur_sym is space
+    @p const_space xor
+    if space_seen_set
+
+    \ branch if space was encountered
+    @p space_seen lit -1 +
+    if check_higher_a
+
+    check_higher_A ;
+
+
+    \ set space_seen and continue
+space_seen_set:
+    lit 1 !p space_seen
+    drop
+    title_while_end ;
+
+check_higher_a:
+    dup                      \ cur_sym:cur_sym:input_remaining:[]
+    @p const_a_neg +         \ cur_sym - const_a:cur_sym:input_remaining:[]
+    -if check_lower_z        \ branch if cur_sym >= const_a
+    drop
+    continue ; ;
+check_lower_z:
+    dup inv lit 1 +          \ -cur_sym:cur_sym:input_remaining:[]
+    @p const_z +             \ const_z - cur_sym:cur_sym:input_remaining:[]
+    -if capitalize_char      \ branch if cur_sym <= const_z
+    drop
+    continue ; ;
+
+check_higher_A:
+    dup                      \ cur_sym:cur_sym:input_remaining:[]
+    @p const_A_neg +         \ cur_sym - const_A:cur_sym:input_remaining:[]
+    -if check_lower_Z        \ branch if cur_sym >= const_A
+    drop
+    continue ; ;
+check_lower_Z:
+    dup inv lit 1 +          \ -cur_sym:cur_sym:input_remaining:[]
+    @p const_Z +             \ const_Z - cur_sym:cur_sym:input_remaining:[]
+    -if lowercase_char       \ branch if cur_sym <= const_Z
+    drop
+    continue ; ;
+
+capitalize_char:
+    lit -32 +                \ cur_sym-32:input_remaining:[]
+    write_first_byte
+    continue ;
+
+lowercase_char:
+    lit 32 +
+    write_first_byte
+
+continue:
+    lit 0 !p space_seen
+title_while_end:
+
+    lit 1 a + a!             \ move pointer a -> a + 1
+
+    next title_while 
     ;
+title_finish:
+    ;
+
+    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 write_output:
-    @p output_addr b!
-    @+ lit 255 and           \ buf_size:[]
+    @p output_addr b!        \ b for output
+    lit buf a!               \ a for input
+
+    @+ lit 255 and
+    lit -1 + >r
 write_output_while:
-    dup                      \ buf_size:buf_size:[]
-    if write_output_finish   \ buf_size:[]
+    @+ lit 255 and           \ cur_sym:[]
 
-    @+ lit 255 and           \ cur_sym:buf_size:[]
+    !b                       \ []
 
-    !b                       \ buf_size:[]
-
-    lit -1 +                 \ buf_size - 1:[]
-
-    write_output_while ;
-
-write_output_finish:
-    drop                     \ remove remaining zero counter
+    next write_output_while
     ;
+
+    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+    \ since current arch is little-endian, i.e., mem[0x00]=0x0f, mem[0x01]=0x0e, mem[0x02]=0x0d, mem[0x03]=0x0c
+    \ will become 0x0c0d0e0f on read from address 0x00
+write_first_byte:
+    lit 0xffffff00 @         \ a_value:mask:input:[]
+
+    and                      \ masked_value:input:[]
+    xor !                    \ rewrite first byte
+    ;
+
+    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 error:
     @p output_addr b!
-    lit -1 !b                \ write -1 to output
+    lit -858993460 !b        \ write error to output
     halt
